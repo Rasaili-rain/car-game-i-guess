c4d18a988efd112c6dc093992357ebdf8613f8a1
Rasaili-rain <saditrasaili063@gmail.com>
2026-04-23 22:50:44 +0545

    image

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from turtle import Turtle,Screen
+from turtle import Screen
 import time
 from cars import Cars
 from player import Player
 from scoreboard import Scoreboard
 
-FINISH_LINE =  280
+FINISH_LINE = 280
 speed = 0.05
 
 screen = Screen()
-screen.setup(height=600,width=800)
+screen.setup(height=600, width=800)
 screen.tracer(0)
 
 cars = Cars()
@@ -20,18 +20,18 @@ cars.create_cars()
 
 game_is_on = True
 screen.listen()
-screen.onkeypress(player.go_up,"Up")
-screen.onkeypress(player.go_down,"Down")
+screen.onkeypress(player.go_up, "Up")
+screen.onkeypress(player.go_down, "Down")
 
 while game_is_on:
     time.sleep(0.2)
     screen.update()
-    if cars.move_cars(player.player.position()) == True:
+    if cars.move_cars(player.player.position()):
         game_is_on = False
         screen.clear()
         scoreboard.game_over()
     if player.player.ycor() >= FINISH_LINE:
-        player.player.goto(0,-280)
+        player.player.goto(0, -280)
         speed *= 0.5
         scoreboard.update_score()
 
